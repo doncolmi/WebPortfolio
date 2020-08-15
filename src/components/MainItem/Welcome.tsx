5ace1060c4e6b527d96a4fec15ec716fff45b788
doncolmi <matathresh1740@naver.com>
2020-08-15 17:41:43 +0900

    add MainPage

--- src/components/MainItem/Welcome.tsx
+++ src/components/MainItem/Welcome.tsx
@@ -5,38 +5,25 @@ import "./Welcome.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-
 const Welcome: FC = () => {
   AOS.init();
 
   return (
     <div className="wrapper section wrapImg">
       <div className="wrapper">
+        <div className="video w100h100">
+          <video muted autoPlay loop>
+            <source src="/Sand.mp4" type="video/mp4"></source>
+          </video>
+        </div>
         <div className="halfOpacity w100h100" />
-        <div className="contents w100h100 flexColumn flexCenter">
-          <div
-            className="title"
-            data-aos="fade-up"
-            data-aos-delay="100"
-            data-aos-duration="1000"
-          >
-            임대성 포트폴리오
+        <div className="contents w100h100 welcome">
+          <div className="Hello" data-aos="fade-right" data-aos-duration="1000">
+            <div className="Name HelloBox">임대성</div>
+            <div className="Portfolio HelloBox">포트폴리오</div>
+            <div className="school">한국폴리텍대학 데이터분석과</div>
           </div>
-          <div
-            className="exp"
-            data-aos="fade-up"
-            data-aos-delay="300"
-            data-aos-duration="1000"
-          >
-            한국폴리텍대학 데이터분석과
-          </div>
-          <div 
-            className="login" 
-            data-aos="fade-up"
-            data-aos-delay="500"
-            data-aos-duration="1000"
-          >
-          </div>
+          <div></div>
         </div>
       </div>
     </div>
